f9db0267380d92c2d0394ed58c6c58bb067e2ce6
zhanghaidi <zhanghaidi@guangquaner.com>
2016-09-07 15:28:45 +0800

    Initial Commit

---
+++ YLPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLPlayer
+//
+//  Created by zhanghaidi on 16/9/7.
+//  Copyright © 2016年 yanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
